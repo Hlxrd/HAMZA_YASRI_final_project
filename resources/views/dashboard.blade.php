8c74a15db5f7f72fd06d09a42ce63399d2c82d9f
Hlxrd1~ <hamzayasri359@gmail.com>
2024-05-02 04:40:57 +0200

    latest commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -69,7 +69,7 @@
                                         <button
                                             class="outline  px-3 py-1.5 my-4 mx-3 hover:scale-105 transition-all      rounded outline-blue-400 bg-slate-800 text-blue-200  hover:bg-slate-600 hover:outline-red-700 hover:text-red-700">Delete</button>
                                     </form>
-                                @elseif (Auth::user()->role == 'User' )
+                                {{-- @elseif (Auth::user()->role == 'User' )
                                     <a href="{{ route("PostRentals.show" , $rental) }}"
                                         class="inline-flex  cursor-pointer h-[20%] items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         Read more
@@ -79,9 +79,9 @@
                                                 stroke-linejoin="round" stroke-width="2"
                                                 d="M1 5h12m0 0L9 1m4 4L9 9" />
                                         </svg>
-                                    </a>
+                                    </a> --}}
                                     @else
-                                    <a href="/rental" id="{{ $rental->id }}"
+                                    <a href="/rental" 
                                         class="inline-flex  cursor-pointer h-[20%] items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         Read more
                                         <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
@@ -90,8 +90,8 @@
                                                 stroke-linejoin="round" stroke-width="2"
                                                 d="M1 5h12m0 0L9 1m4 4L9 9" />
                                         </svg>
-                                    </>
-                                @endif
+                                    </a>
+                                @endif 
                             </div>
 
                         </div>
